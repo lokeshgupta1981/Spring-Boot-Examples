254b21272962fe8b13ebe36255ff9cfa3d673647
lokeshgupta1981 <howtodoinjava@gmail.com>
2022-06-17 12:18:36 +0530

    Initial Commit

---
+++ src/main/java/com/howtodoinjava/demo/App.java
@@ -0,0 +1,14 @@
+package com.howtodoinjava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
