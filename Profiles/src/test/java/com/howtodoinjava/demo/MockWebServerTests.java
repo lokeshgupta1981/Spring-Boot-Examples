e8811cbf43b04c302c8d5984f0415af79b37ae68
lokeshgupta1981 <howtodoinjava@gmail.com>
2022-06-21 15:40:49 +0530

    OkHttp MockWebServer Demo

--- Profiles/src/test/java/com/howtodoinjava/demo/MockWebServerTests.java
+++ Profiles/src/test/java/com/howtodoinjava/demo/MockWebServerTests.java
@@ -1,6 +1,5 @@
 package com.howtodoinjava.demo;
 
-
 import okhttp3.mockwebserver.Dispatcher;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
@@ -22,46 +21,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class MockWebServerTests {
   public static MockWebServer server;
-
-  @BeforeAll
-  static void setUp() throws IOException {
-    server = new MockWebServer();
-    server.setDispatcher(dispatcher);
-    server.start(8080);
-  }
-
-  @AfterAll
-  static void tearDown() throws IOException {
-    server.shutdown();
-  }
-
-  @Test
-  void simpleTest() throws InterruptedException {
-
-    WebClient webClient = WebClient.create(String.format("http://%s:8080",
-        server.getHostName()));
-
-    Mono<String> apiResponse = webClient.post()
-        .uri("/api-url-two")
-        .body(Mono.just("<data />"), String.class)
-        .header("Authorization", "Basic " + Base64Utils
-            .encodeToString(("username:password").getBytes(UTF_8)))
-        .retrieve()
-        .bodyToMono(String.class);
-
-    //Validate Response
-    StepVerifier.create(apiResponse)
-        .expectNext("<response />")
-        .verifyComplete();
-
-    //Verify API HIT
-    RecordedRequest request = server.takeRequest();
-    assertEquals("/api-url-two", request.getPath());
-    assertEquals("POST", request.getMethod());
-    assertNotNull(request.getHeader("Authorization"));
-    assertEquals("<data />", request.getBody().readUtf8());
-  }
-
   final static Dispatcher dispatcher = new Dispatcher() {
 
     @Override
@@ -81,7 +40,7 @@ public class MockWebServerTests {
         case "/api-url-three":
           return new MockResponse()
               .setResponseCode(500)
-              .setBodyDelay(5000, TimeUnit.SECONDS)
+              .setBodyDelay(5000, TimeUnit.MILLISECONDS)
               .setChunkedBody("<error-response />", 5);
 
         case "/api-url-four":
@@ -94,4 +53,42 @@ public class MockWebServerTests {
     }
   };
 
+  @BeforeAll
+  static void setUp() throws IOException {
+    server = new MockWebServer();
+    server.setDispatcher(dispatcher);
+    server.start(8080);
+  }
+
+  @AfterAll
+  static void tearDown() throws IOException {
+    server.shutdown();
+  }
+
+  @Test
+  void simpleTest() throws InterruptedException {
+
+    WebClient webClient = WebClient
+        .create(String.format("http://%s:8080", server.getHostName()));
+
+    Mono<String> apiResponse = webClient.post()
+        .uri("/api-url-two")
+        .body(Mono.just("<data />"), String.class)
+        .header("Authorization", "Basic " +
+            Base64Utils.encodeToString(("username:password").getBytes(UTF_8)))
+        .retrieve()
+        .bodyToMono(String.class);
+
+    //Validate Response
+    StepVerifier.create(apiResponse)
+        .expectNext("<response />")
+        .verifyComplete();
+
+    //Verify API HIT
+    RecordedRequest request = server.takeRequest();
+    assertEquals("/api-url-two", request.getPath());
+    assertEquals("POST", request.getMethod());
+    assertNotNull(request.getHeader("Authorization"));
+    assertEquals("<data />", request.getBody().readUtf8());
+  }
 }
