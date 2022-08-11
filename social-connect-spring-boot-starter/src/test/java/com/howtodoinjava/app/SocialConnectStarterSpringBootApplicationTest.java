d289ba87beff9feb00f427184353f3c5899f8049
lokeshgupta1981 <howtodoinjava@gmail.com>
2022-08-11 12:36:48 +0530

    Removed junit 4

--- social-connect-spring-boot-starter/src/test/java/com/howtodoinjava/app/SocialConnectStarterSpringBootApplicationTest.java
+++ social-connect-spring-boot-starter/src/test/java/com/howtodoinjava/app/SocialConnectStarterSpringBootApplicationTest.java
@@ -3,17 +3,14 @@ package com.howtodoinjava.app;
 import com.howtodoinjava.app.service.SocialConnectService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class SocialConnectStarterSpringBootApplicationTest {
-  
+
   @Autowired
   private SocialConnectService socialConnectService;
 
