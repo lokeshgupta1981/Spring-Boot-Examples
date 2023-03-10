a0a4758754acce6fe5035f2c06ce863e2c88d671
lokeshgupta1981 <howtodoinjava@gmail.com>
2023-03-10 15:14:34 +0530

    Multiple datasources

--- multiple-dataSources/src/test/java/com/howtodoinjava/app/AppTest.java
+++ multiple-dataSources/src/test/java/com/howtodoinjava/app/AppTest.java
@@ -5,7 +5,6 @@ import com.howtodoinjava.app.model.order.Order;
 import com.howtodoinjava.app.model.user.User;
 import com.howtodoinjava.app.repositories.order.OrderRepository;
 import com.howtodoinjava.app.repositories.user.UserRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,31 +21,14 @@ public class AppTest {
 
   @Autowired
   private OrderRepository orderRepository;
+  
+  @Test
+  void saveUserDataInDB() {
 
-  User user;
-  Order order;
-
-  @BeforeEach
-  public void setup() {
-    user = new User();
-    order = new Order();
-
+    User user = new User();
     user.setEmail("johndoe@gmail.com");
     user.setFirstName("John");
     user.setLastName("DOe");
-
-    order.setProductName("Mobile");
-    order.setOrderAmount(200);
-    order.setUserId(1);
-  }
-
-  @Test
-  void contextLoads() {
-  }
-
-
-  @Test
-  void saveUserDataInDB() {
 
     User savedUser = userRepository.save(user);
     Optional<User> userData = userRepository.findById(savedUser.getId());
@@ -56,6 +38,11 @@ public class AppTest {
 
   @Test
   void saveOrderDataInDB() {
+
+    Order order = new Order();
+    order.setProductName("Mobile");
+    order.setOrderAmount(200);
+    order.setUserId(1);
 
     Order savedOrder = orderRepository.save(order);
     Optional<Order> orderData = orderRepository.findById(savedOrder.getId());
