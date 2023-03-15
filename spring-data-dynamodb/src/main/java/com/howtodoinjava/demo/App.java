25487c0e571a4a03491c15dd2d09b2b3d072be31
lokeshgupta1981 <howtodoinjava@gmail.com>
2023-03-15 14:42:13 +0530

    Code Refactor

--- spring-data-dynamodb/src/main/java/com/howtodoinjava/demo/App.java
+++ spring-data-dynamodb/src/main/java/com/howtodoinjava/demo/App.java
@@ -1,9 +1,19 @@
 package com.howtodoinjava.demo;
 
+import com.howtodoinjava.demo.repositories.MovieDetailRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+/**
+ * Below code excludes spring jpa and h2 as to assume it as SQL tables
+ */
+@EnableJpaRepositories(excludeFilters =
+@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value =
+    MovieDetailRepository.class))
 public class App {
 
   public static void main(String[] args) {
--- spring-data-dynamodb/src/test/java/com/howtodoinjava/demo/AppTests.java
+++ spring-data-dynamodb/src/test/java/com/howtodoinjava/demo/AppTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class AppTests {
 
   @Test
-  void contextLoads() {
+  void contextLoads() throws Exception {
   }
 
 }
