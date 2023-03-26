8b1bcc29080157439fb5c301e433658971023560
lokeshgupta1981 <howtodoinjava@gmail.com>
2023-03-26 22:18:45 +0530

    MongoDb Config Demo

---
+++ spring-boot-mongodb-crud/src/test/java/com/howtodoinjava/app/AppTest.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTest {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
