1a64dc5cab3fb393072286820a97b4bcf3626e17
saumil11-coder <84261342+saumil11-coder@users.noreply.github.com>
2023-04-26 10:03:01 +0530

    Add files via upload

---
+++ uploading (1)/uploading/src/main/java/com/springapi/uploading/UploadingApplication.java
@@ -0,0 +1,13 @@
+package com.springapi.uploading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadingApplication.class, args);
+	}
+
+}
