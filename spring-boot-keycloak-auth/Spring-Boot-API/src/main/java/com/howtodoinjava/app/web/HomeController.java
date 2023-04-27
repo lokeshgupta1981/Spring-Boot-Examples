e38526f8bf63f6e9c839129708fe896aa5ce32e2
hamza <hamza.nassour13@gmail.com>
2023-04-27 16:04:27 +0100

    spring-docker-aws module

--- spring-boot-keycloak-auth/Spring-Boot-API/src/main/java/com/howtodoinjava/app/web/HomeController.java
+++ spring-boot-keycloak-auth/Spring-Boot-API/src/main/java/com/howtodoinjava/app/web/HomeController.java
@@ -1,7 +1,16 @@
 package com.howtodoinjava.app.web;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.oidc.authentication.OidcAuthorizationCodeAuthenticationProvider;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Iterator;
+import java.util.List;
 
 @RestController
 public class HomeController {
@@ -11,8 +20,13 @@ public class HomeController {
     public String publicResource(){
         return "public Resource ";
     }
-    @GetMapping("/protected")
-    public String protectedResource(){
-        return "protected Resource ";
+    @GetMapping("/")
+    public String protectedResource(HttpSession httpSession ){
+
+        Iterator<String> iterator = httpSession.getAttributeNames().asIterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+        return "protected Resource " + httpSession.getAttribute("SPRING_SECURITY_CONTEXT");
     }
 }
