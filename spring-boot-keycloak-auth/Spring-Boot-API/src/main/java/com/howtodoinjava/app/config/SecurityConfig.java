89595d9ca893e80931f8dc5c6ffed78a7e3a7a3d
hamza <hamza.nassour13@gmail.com>
2023-04-28 13:22:55 +0100

    Revert "spring-docker-aws module"
    This reverts commit e38526f8bf63f6e9c839129708fe896aa5ce32e2.

--- spring-boot-keycloak-auth/Spring-Boot-API/src/main/java/com/howtodoinjava/app/config/SecurityConfig.java
+++ spring-boot-keycloak-auth/Spring-Boot-API/src/main/java/com/howtodoinjava/app/config/SecurityConfig.java
@@ -4,44 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
-import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeHttpRequests()
+                .requestMatchers("/public").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .oauth2Login();
        return  httpSecurity.build();
-    }
-
-
-
-    @Bean
-    public ClientRegistrationRepository clientRepository() {
-        ClientRegistration keycloak = keycloakClientRegistration();
-        return new InMemoryClientRegistrationRepository(keycloak);
-    }
-
-    private ClientRegistration keycloakClientRegistration() {
-        return ClientRegistration.withRegistrationId("employee-api")
-                .clientId("employee-api")
-                .clientSecret("8SBeGY3JM9xa6F3zdEcu6QHuI08bSVaH")
-                .redirectUri("http://localhost:9090/login/oauth2/code/employee-api")
-                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .issuerUri("http://localhost:8080/auth/realms/howtodoinjava")
-                .authorizationUri("http://localhost:8080/auth/realms/howtodoinjava/protocol/openid-connect/auth")
-                .tokenUri("http://localhost:8080/auth/realms/howtodoinjava/protocol/openid-connect/token")
-                .userInfoUri("http://localhost:8080/auth/realms/howtodoinjava/protocol/openid-connect/userinfo")
-                .userNameAttributeName("sub")
-                .build();
     }
 }
--- spring-boot-keycloak-auth/Spring-Boot-API/src/main/java/com/howtodoinjava/app/web/HomeController.java
+++ spring-boot-keycloak-auth/Spring-Boot-API/src/main/java/com/howtodoinjava/app/web/HomeController.java
@@ -1,16 +1,7 @@
 package com.howtodoinjava.app.web;
 
-import jakarta.servlet.http.HttpSession;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.oidc.authentication.OidcAuthorizationCodeAuthenticationProvider;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Principal;
-import java.util.Iterator;
-import java.util.List;
 
 @RestController
 public class HomeController {
@@ -20,13 +11,8 @@ public class HomeController {
     public String publicResource(){
         return "public Resource ";
     }
-    @GetMapping("/")
-    public String protectedResource(HttpSession httpSession ){
-
-        Iterator<String> iterator = httpSession.getAttributeNames().asIterator();
-        while (iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
-        return "protected Resource " + httpSession.getAttribute("SPRING_SECURITY_CONTEXT");
+    @GetMapping("/protected")
+    public String protectedResource(){
+        return "protected Resource ";
     }
 }
