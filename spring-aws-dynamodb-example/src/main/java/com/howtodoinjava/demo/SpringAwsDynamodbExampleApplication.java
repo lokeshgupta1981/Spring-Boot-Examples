de027685acd4175e8522fd9485483cd9545a93b0
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2023-05-07 12:14:17 +0530

    spring-aws-dynamodb-example

---
+++ spring-aws-dynamodb-example/src/main/java/com/howtodoinjava/demo/SpringAwsDynamodbExampleApplication.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsDynamodbExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsDynamodbExampleApplication.class, args);
+    }
+
+}
