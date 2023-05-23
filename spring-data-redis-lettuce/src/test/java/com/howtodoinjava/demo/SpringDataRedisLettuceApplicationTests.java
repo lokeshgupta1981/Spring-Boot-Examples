53ce2f08cf1964e43ad821b05ddc1b2ddf61cd77
Bayvao Verma <34921943+Bayvao@users.noreply.github.com>
2023-05-23 15:46:53 +0530

    Using test containers to run integration test for redis standalone server configuration

--- spring-data-redis-lettuce/src/test/java/com/howtodoinjava/demo/SpringDataRedisLettuceApplicationTests.java
+++ spring-data-redis-lettuce/src/test/java/com/howtodoinjava/demo/SpringDataRedisLettuceApplicationTests.java
@@ -2,28 +2,54 @@ package com.howtodoinjava.demo;
 
 
 import com.howtodoinjava.demo.service.RedisCacheService;
+import com.redis.testcontainers.RedisContainer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 @SpringBootTest
+@Testcontainers(disabledWithoutDocker = true)
 class SpringDataRedisLettuceApplicationTests {
 
   @Autowired
   private RedisCacheService cacheUtil;
 
+  @Container
+  private static final RedisContainer REDIS_CONTAINER =
+          new RedisContainer(DockerImageName.parse("redis:5.0.3-alpine")).withExposedPorts(6379);
+
   String key;
   String value;
   String hashKey;
+
+  @DynamicPropertySource
+  private static void registerRedisProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.data.redis.host", REDIS_CONTAINER::getHost);
+    registry.add("spring.data.redis.port", () -> REDIS_CONTAINER.getMappedPort(6379)
+            .toString());
+    registry.add("default.redis.connection", () -> "standalone");
+  }
 
   @BeforeEach
   public void setUp() {
     key = "Name";
     value = "Java";
     hashKey = "Subject";
+  }
+
+  @Test
+  void givenRedisContainerConfiguredWithDynamicProperties_whenCheckingRunningStatus_thenStatusIsRunning() {
+    assertTrue(REDIS_CONTAINER.isRunning());
   }
 
   @Test
