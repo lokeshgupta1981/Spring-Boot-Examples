cd1cf0362d78144f4d62337cd3f9100bf6bbb44c
Chandresh Thakkar <tony051970@gmail.com>
2023-05-29 14:23:52 +0530

    AWS Cognito Authentication Added

---
+++ spring-security-aws-cognito-authentication/src/test/java/com/oauth2/cognito/demo/base/AwsCognitoBasicAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2.cognito.demo.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsCognitoBasicAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
