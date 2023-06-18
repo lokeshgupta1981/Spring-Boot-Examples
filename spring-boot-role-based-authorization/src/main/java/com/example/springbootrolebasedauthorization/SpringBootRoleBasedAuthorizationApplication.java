ea3dea99cea19a50f51f72356bee2d8c11b2b164
hamza <hamza.nassour13@gmail.com>
2023-06-18 21:04:47 +0100

    role-based-authorization-module

---
+++ spring-boot-role-based-authorization/src/main/java/com/example/springbootrolebasedauthorization/SpringBootRoleBasedAuthorizationApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootrolebasedauthorization;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SpringBootRoleBasedAuthorizationApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRoleBasedAuthorizationApplication.class, args);
+    }
+}
