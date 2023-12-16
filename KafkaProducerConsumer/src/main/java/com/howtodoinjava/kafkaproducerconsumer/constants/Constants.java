8c4bc4507a778ebf889d24bef9d3296f2f57c260
ThakkarChandresh <thakkarchandresh66@gmail.com>
2023-12-16 20:20:09 +0530

    Revert "Added : Spring Boot Kafka Producer Consumer Code"
    This reverts commit 3e7712651d06dd631c88e3e009d45f3461b28de4.

--- KafkaProducerConsumer/src/main/java/com/howtodoinjava/kafkaproducerconsumer/constants/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.howtodoinjava.kafkaproducerconsumer.constants;
-
-public class Constants
-{
-    public static final String TOPIC_NAME = "howtodoinjava";
-
-    public static final String GROUP_ID = "coders.group";
-}
